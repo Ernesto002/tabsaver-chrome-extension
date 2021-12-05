41ab4bc242bdee01da5e7b8dc27055ef5c26e0a1
ernesto <ernesto002@icloud.com>
2021-12-05 13:30:23 -0800

    use localStorage to persist data that has been entered through the save input button

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const saveInputBtn = document.getElementById("save-input-btn")
 const saveTabBtn = document.getElementById("save-tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+const tabsFromLocalStorage = JSON.parse( localStorage.getItem("myTabs") )
+
+if (tabsFromLocalStorage){
+    myTabs = tabsFromLocalStorage
+    render(myTabs)
+}
 
 saveInputBtn.addEventListener("click", function(){
     myTabs.push(textEl.value)
     textEl.value = ""
+    localStorage.setItem("myTabs", JSON.stringify(myTabs))
     render(myTabs)
 })
 
@@ -24,7 +31,7 @@ deleteBtn.addEventListener("click", function(){
     render(myTabs)
 })
 
-function render(tabs) {
+function render(tabs){
     let listTabs = ""
     for(i = 0; i < tabs.length; i++){
         listTabs += `
