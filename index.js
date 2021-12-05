d973a8b675ed54edac36e770ec99ff69046927eb
ernesto <ernesto002@icloud.com>
2021-12-05 12:54:48 -0800

    initialize empty myTabs array, grab elements from DOM

---
+++ index.js
@@ -0,0 +1,6 @@
+let myTabs = []
+const textEl = document.getElementById("text-el")
+const inputBtn = document.getElementById("input-btn")
+const saveBtn = document.getElementById("save-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const ulEl = document.getElementById("ul-el")
